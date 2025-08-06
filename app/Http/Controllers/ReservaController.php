44a6ddfc083eab1f80dbd6ca0c512f4550b872f8
Marto <martincoppa15@gmail.com>
2025-08-05 21:26:03 -0300

    avances reserva interna

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Persona;
 use App\Models\Reserva;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReservaController extends Controller
 {
@@ -26,9 +28,23 @@ class ReservaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, int $cancha)
     {
-        //
+        $persona = Persona::whereRelation('contactos', 'descripcion', $request->email)
+            ->with('contactos', 'sexo', 'documentos')
+            ->first();
+
+        if (!$persona) {
+            DB::transaction(function() use($request, $cancha) {
+                $reservante = $this->crearReservante($request);
+                $reserva = $this->crearReserva($request, $reservante, $cancha);
+            });
+        } else {
+            $reservante = $persona;
+            $reserva = $this->crearReserva($request, $reservante, $cancha);
+            return "lo mismo pero en la orta salida";
+        }
+        return "retornamos alguna vista o redireccion";
     }
 
     /**
@@ -61,5 +77,34 @@ class ReservaController extends Controller
     public function destroy(Reserva $reserva)
     {
         //
+    }
+
+    /**
+     * Crear una nueva persona internamente para poder reservar
+     */
+    private function crearReservante(Request $request) :Persona 
+    {
+        $persona = Persona::create([
+            'nombre' => $request->input('nombre'),
+            'email' => $request->input('email'),
+            // otros campos si tenés...
+        ]);
+
+        $persona->contactos()->create([
+            'descripcion' => $request->input('email'),
+            'tipo_contacto_id' => 1, // ejemplo
+        ]);
+
+        $persona->documentos()->create([
+            'numero' => null,
+            'tipo_documento_id' => 1, // o el default
+        ]);
+
+        return $persona;
+    }
+
+    private function crearReserva(Request $request, $reservante, $cancha)
+    {
+        //falta hacer la logica
     }
 }
