7af47d5e6103a9c8bca54c478ae1fa8b3392b828
Martin-PC <martincoppa15@gmail.com>
2025-08-21 19:28:19 -0300

    nullables on reserva

--- database/migrations/2025_06_10_152041_create_reserva_table.php
+++ database/migrations/2025_06_10_152041_create_reserva_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
     {
         Schema::create('reserva', function (Blueprint $table) {
             $table->id();
-            $table->string('observacion');
+            $table->string('observacion')->nullable();
             $table->date('fecha');
             $table->time('hora_desde');
             $table->time('hora_hasta');
-            $table->decimal('precio', 10, 2);
+            $table->decimal('precio', 10, 2)->nullable();
             $table->enum('estado', ['Confirmada', 'Pendiente', 'Cancelada','Completada'])->default('Pendiente');
             $table->enum('metodo_pago', ['Efectivo', 'Tarjeta', 'Transferencia', 'Pendiente'])->default('Pendiente');
             $table->enum('tipo_reserva', ['Interna', 'Externa'])->default('Interna');
-            $table->text('cancelacion_motivo');
-            $table->integer('creado_por');
+            $table->text('cancelacion_motivo')->nullable();
+            $table->integer('creado_por')->nullable();
             $table->foreignId('rela_persona')->constrained('persona')->onDelete('restrict');
             $table->foreignId('rela_zona')->constrained('zona')->onDelete('restrict');
             // $table->foreignId('rela_estado_reserva')->constrained('estado_reserva')->onDelete('restrict');
