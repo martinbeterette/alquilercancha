363ade454c4939248a281d5eaf98a039ce6d5a1d
martin beterette <beterettemartin66@gmail.com>
2025-09-01 17:39:31 -0300

    ultimo commit

--- resources/views/livewire/roles/role-index.blade.php
+++ resources/views/livewire/roles/role-index.blade.php
@@ -24,21 +24,6 @@
                 </div>
             @endif
 
-            {{-- @session('success')
-                <div 
-                    x-data="{ show: true }" 
-                    x-init="setTimeout(() => show = false, 5000)" 
-                    x-show="show"
-                    class="inline-flex items-center p-2 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-green-900 dark:text-green-300 dark:border-green-800 transition-opacity duration-500"
-                    role="alert"
-                >
-                    <svg class="flex-shrink-0 w-8 h-8 mr-1 text-green-700 dark:text-green-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4"></path>
-                    </svg>
-                    <span class="font-medium">{{ $value }}</span>
-                </div>
-            @endsession --}}
-
             <div>
                 @can('role.create')
                     <a href="{{ route("roles.create") }}" class="mb-4 px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
