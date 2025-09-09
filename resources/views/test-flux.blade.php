1d5eb4a4a50754bd80f11106438397c1c3586432
Martin-PC <martincoppa15@gmail.com>
2025-09-09 20:55:39 -0300

    cambios en el layout base

---
+++ resources/views/test-flux.blade.php
@@ -0,0 +1,3 @@
+<x-layout-base>
+    <x-flux-button label="Mi botón" class="bg-red"> Holaaa</x-flux-button>
+</x-layout-base>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-flux', fn () => view('test-flux'));
 require __DIR__.'/routes_breeze.php';
 require __DIR__.'/routes.php';
